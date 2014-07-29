2787bfa813bf1e121304de6158ceae42da8fd162
Kunshan Wang <wks1986@gmail.com>
2014-07-30 01:22:25 +1000

    Making reader specs abstract.

---
+++ src/test/scala/uvm/ir/textinput/AbstractReaderSpec.scala
@@ -0,0 +1,26 @@
+package uvm.ir.textinput
+
+import org.scalatest._
+import uvm._
+
+trait AbstractReaderSpec extends FlatSpec with Matchers {
+  
+  def parseFile(fileName: String): Bundle
+  
+  def theSubject: String
+  
+  behavior of theSubject
+
+  it should "read simple type definitions" in {
+    val b = parseFile("tests/uvm-parsing-test/types.uir")
+  }
+  it should "read simple constant definitions" in {
+    val b = parseFile("tests/uvm-parsing-test/constants.uir")
+  }
+  it should "read simple function definitions" in {
+    val b = parseFile("tests/uvm-parsing-test/functions.uir")
+  }
+  it should "read simple instruction definitions" in {
+    val b = parseFile("tests/uvm-parsing-test/instructions.uir")
+  }
+}
--- src/test/scala/uvm/ir/textinput/UvmIRReaderSpec.scala
+++ src/test/scala/uvm/ir/textinput/UvmIRReaderSpec.scala
@@ -1,27 +1,16 @@
 package uvm.ir.textinput
 
-import org.scalatest.FlatSpec
-import org.scalatest.Matchers
+import org.scalatest._
 
 import uvm.Bundle
 
-class UvmIRReaderSpec extends FlatSpec with Matchers {
+class UvmIRReaderSpec extends AbstractReaderSpec {
+  
+  override def theSubject = "UvmIRReader"
 
-  def parseFile(fileName: String): Bundle = {
+  override def parseFile(fileName: String): Bundle = {
     UvmIRReader.read(new java.io.FileReader(fileName))
   }
 
-  "The IRParser" should "parse simple type definitions" in {
-    parseFile("tests/uvm-parsing-test/types.uir")
-  }
-  it should "parse simple constant definitions" in {
-    parseFile("tests/uvm-parsing-test/constants.uir")
-  }
-  it should "parse simple function definitions" in {
-    parseFile("tests/uvm-parsing-test/functions.uir")
-  }
-  it should "parse simple instruction definitions" in {
-    parseFile("tests/uvm-parsing-test/instructions.uir")
-  }
 
 }
