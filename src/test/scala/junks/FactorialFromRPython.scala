13ddfc1959fb008abd92709ee2a94dc9a0ff575f
Kunshan Wang <wks1986@gmail.com>
2014-12-22 13:27:38 +1100

    Added an externally generated uir factorial program.

---
+++ src/test/scala/junks/FactorialFromRPython.scala
@@ -0,0 +1,43 @@
+package junks
+
+import uvm.refimpl._
+
+object FactorialFromRPython extends App {
+  val microVM = new MicroVM()
+
+  val r = new java.io.FileReader("tests/extra-progs/factorial.uir")
+
+  val ca = microVM.newClientAgent()
+  ca.loadBundle(r)
+
+  // Magical trick. Theoretically the client would publish bundles as binary and knows all the IDs. But in this version
+  // only the text form is supported and IDs are automatically generated. So we look into the globalBundle itself.
+  val m = ca.putFunction(microVM.globalBundle.funcNs("@main").id)
+
+  microVM.trapManager.trapHandler = new TrapHandler {
+    override def handleTrap(ca: ClientAgent, thread: Handle, stack: Handle, watchPointID: Int): TrapHandlerResult = {
+      val curInst = ca.currentInstruction(stack, 0)
+      val trapName = microVM.globalBundle.varNs(curInst).name.get
+
+      if (trapName == "@main_v1.main_trap") {
+        val kas = ca.dumpKeepalives(stack, 0)
+        val Seq(rv) = kas
+
+        val i = ca.toInt(rv, signExt = true)
+
+        println(i)
+      } else {
+        throw new RuntimeException("Hit the wrong trap: " + trapName)
+      }
+
+      TrapRebindPassVoid(stack) // continue
+    }
+  }
+
+  val sta = ca.newStack(m, Seq())
+  val thr = ca.newThread(sta)
+
+  microVM.threadStackManager.joinAll() // run until all threads stop
+
+  ca.close()
+}
