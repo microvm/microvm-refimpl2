44f2b0c9254b46747a4b4ed6d337c3fac355b984
Kunshan Wang <wks1986@gmail.com>
2015-02-25 18:04:48 +1100

    Updated Interact.scala

--- src/test/scala/tutorial/Interact.scala
+++ src/test/scala/tutorial/Interact.scala
@@ -4,6 +4,8 @@ import uvm.refimpl._
 
 object Interact extends App {
   val microVM = new MicroVM()
+  
+  implicit def idOf(name: String) = microVM.idOf(name)
 
   val ca = microVM.newClientAgent()
 
@@ -11,7 +13,21 @@ object Interact extends App {
   ca.loadBundle(reader)
   reader.close()
   
-  implicit def idOf(name: String) = microVM.idOf(name)
+  object MyTrapHandler extends TrapHandler {
+    def handleTrap(ca: ClientAgent, thread: Handle, stack: Handle, watchPointID: Int): TrapHandlerResult = {
+      val curInstID = ca.currentInstruction(stack, 0)
+      microVM.nameOf(curInstID) match {
+        case "@main_v1.the_trap" => {
+          val Seq(result) = ca.dumpKeepalives(stack, 0)
+          val resultInt = ca.toInt(result, signExt = true)
+          printf("The result is %d.\n", resultInt)
+          TrapRebindPassVoid(st)
+        }
+      }
+    }
+  }
+  
+  microVM.trapManager.trapHandler = MyTrapHandler
 
   val mainFunc = ca.putFunction("@main")
   val st = ca.newStack(mainFunc, Seq())
